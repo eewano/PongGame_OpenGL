41ff26b9d140408a18ab7b8fed28d428d3e5927d
Awano Yasutaka <eewano@gmail.com>
2017-07-22 17:33:40 +0900

    1セットにおけるポイント数が間違っていた為、修正。

--- PongGame/Define.hpp
+++ PongGame/Define.hpp
@@ -6,6 +6,6 @@
 static Vec2i WINDOW_SIZE = { 800, 600 };
 static const float ASPECT_RATIO = static_cast<float>(WINDOW_SIZE.x) / WINDOW_SIZE.y;
 static constexpr int INTERVAL = 60;
-static constexpr int WIN_SCORE = 2;
+static constexpr int WIN_SCORE = 3;
 
 #endif /* Define_h */
